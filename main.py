478b6817fe102daf19c60c6d950f8282497af7b6
brianyildirim <byildiri@uci.edu>
2025-01-16 03:41:34 -0800

    Formatting UI.

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ class WeatherApp(QWidget):
         self.temperature_label = QLabel("70°F", self)
         self.emoji_label = QLabel("☀️", self)
         self.description_label = QLabel("Sunny", self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Weather App")
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+        self.setLayout(vbox)
 
 
 if __name__ == '__main__':
