0d708d30db313c02e1a096d5874c4dbf5512483a
brianyildirim <byildiri@uci.edu>
2025-01-16 03:29:16 -0800

    Imported necessary modules.

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+import requests
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
+                             QLineEdit, QPushButton, QVBoxLayout)
